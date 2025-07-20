35257db5788b5b63bb143c0c888264e3159a1a78
Damon Cheung <damoncht@umich.edu>
2025-07-20 02:32:57 -0700

    multi glitches

---
+++ .ipynb_checkpoints/sim-checkpoint.py
@@ -0,0 +1,87 @@
+import lal
+from lalpulsar import simulateCW
+import time 
+from tqdm import tqdm
+import numpy as np 
+
+def waveform(h0, cosi, freq, f1dot, f2dot, f3dot, f4dot, tglitch, df_permanent, df_tau, df1_permanent, df1_tau, tau):
+    # Validate that all glitch-related parameters are lists of the same length
+    if not all(len(lst) == len(tglitch) for lst in [df_permanent, df_tau, df1_permanent, df1_tau, tau]):
+        raise ValueError("All glitch parameter lists must have the same length")
+    
+    def wf(dt):
+        # Base phase calculation
+        dphi = freq * dt + f1dot * 0.5 * dt**2 + f2dot * (1./6.) * dt**3 + \
+               f3dot * (1./24.) * dt**4 + f4dot * (1./120.) * dt**5
+        
+        # Add contributions from each glitch
+        for i in range(len(tglitch)):
+            if dt > tglitch[i]:
+                dphi += df_tau[i] * np.exp(-(dt - tglitch[i]) / tau[i]) * (dt - tglitch[i])
+                dphi += df_permanent[i] * (dt - tglitch[i])
+                dphi += df1_tau[i] * np.exp(-(dt - tglitch[i]) / tau[i]) * 0.5 * (dt - tglitch[i])**2
+                dphi += df1_permanent[i] * 0.5 * (dt - tglitch[i])**2
+        
+        dphi = lal.TWOPI * dphi
+        
+        # Amplitude calculations remain unchanged
+        ap = h0 * (1.0 + cosi**2) / 2.0
+        ax = h0 * cosi
+        
+        return dphi, ap, ax
+    return wf
+ 
+
+tstart   = 1368970000
+Tdata    = 100 * 86400
+tref     = tstart + 0.5 * Tdata
+h0       = 1e-24
+cosi     = 1
+psi      = 0
+phi0     = 3.210
+freq     = 20.0
+f1dot    = -1.35e-8
+f2dot    = 0
+f3dot    = 0
+f4dot    = 0
+
+tglitch = [tstart + 0.4 * Tdata, tstart + 0.7 * Tdata]
+tglitch /= tref
+df_permanent = [5e-2, 5e-2]
+df_tau = [5e-2, 5e-2]
+df1_permanent = [-5e-11, -5e-11]
+df1_tau = [-5e-11, -5e-11]
+tau = [20*86400, 20*86400]
+
+dt_wf    = 5
+alpha    = 6.12
+delta    = 1.02
+detector = 'H1'
+ 
+t0 = time.time()
+wf = waveform(h0, cosi, freq, f1dot, f2dot, f3dot, f4dot, tglitch, df_permanent, df_tau, df1_permanent, df1_tau, tau)
+S = simulateCW.CWSimulator(tref, tstart, Tdata, wf, dt_wf, phi0, psi, alpha, delta, detector)
+ 
+# To write SFT files
+for file, i, N in tqdm(S.write_sft_files(fmax=21, Tsft=1800, comment="simCW", out_dir='./sfts/'), total=Tdata / 86400 * 48):
+    #print('Generated SFT file %s (%i of %i)' % (file, i+1, N))
+    continue 
+    
+print("Done. Time used: {}s".format(time.time()-t0))
+# To write frame files
+# for file, i, N in S.write_frame_files(fs=1, Tframe=1800, comment="simCW", out_dir='./sfts/'):
+#     print('Generated frame file %s (%i of %i)' % (file, i+1, N))
+
+
+# Lists for multiple glitches
+tglitch = [0, 30 * 86400, 60 * 86400]  # Glitch times
+df_permanent = [5e-2, 3e-2, 2e-2]      # Permanent frequency changes
+df_tau = [5e-2, 4e-2, 3e-2]            # Transient frequency changes
+df1_permanent = [-5e-11, -3e-11, -2e-11]  # Permanent fdot changes
+df1_tau = [-5e-11, -4e-11, -3e-11]     # Transient fdot changes
+tau = [20 * 86400, 15 * 86400, 10 * 86400]  # Decay timescales
+dt_wf = 5
+alpha = 6.12
+delta = 1.02
+detector = 'H1'
+
